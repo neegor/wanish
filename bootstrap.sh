137823a270bc9c0854efc112874a62f6bff25161
neegor <gorschal@gmail.com>
2015-03-27 17:08:10 +0300

    add zlib1g-dev

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 sudo apt-get update
 sudo apt-get -y install  python3-setuptools
 sudo easy_install3 -U pip
-sudo apt-get -y install python3-dev libxml2-dev libxslt-dev
+sudo apt-get -y install python3-dev libxml2-dev libxslt-dev zlib1g-dev
 sudo pip3 install requests lxml cssselect chardet numpy snowballstemmer networkx
