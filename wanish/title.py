29e4e569b98b0f6f72c51082bf0f59048c710724
byakuya <mukhin.vladimir@googlemail.com>
2015-06-28 20:44:50 +0300

    Removed unused code.

--- wanish/title.py
+++ wanish/title.py
@@ -37,34 +37,6 @@ def add_match(collection, text, orig):
             collection.add(text)
 
 
-# http://stackoverflow.com/questions/22726177/longest-common-substring-without-cutting-a-word-python
-def longest_common_substring(s1, s2):
-    m = [[0] * (1 + len(s2)) for i in range(1 + len(s1))]
-    longest, x_longest = 0, 0
-    for x in range(1, 1 + len(s1)):
-        for y in range(1, 1 + len(s2)):
-            if s1[x - 1] == s2[y - 1]:
-                m[x][y] = m[x - 1][y - 1] + 1
-                if m[x][y] > longest:
-                    longest = m[x][y]
-                    x_longest = x
-            else:
-                m[x][y] = 0
-    return s1[x_longest - longest: x_longest]
-
-
-def longest_common_sentence(s1, s2):
-    """
-    Finds longest common substring of words from two strings
-    :param s1: first string
-    :param s2: second string
-    :return: longest words sequence of the two strings
-    """
-    s1_words = s1.split(' ')
-    s2_words = s2.split(' ')
-    return ' '.join(longest_common_substring(s1_words, s2_words))
-
-
 def shorten_title(doc):
 
     # looking for tag containing itemprop='headline' first
