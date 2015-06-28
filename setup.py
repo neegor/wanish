943c4976d0f48ec9d620d2b4580568ad7ce85e25
neegor <gorschal@gmail.com>
2015-06-28 20:54:35 +0300

    version 0.4.8

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.platform == 'darwin':
 
 setup(
     name="wanish",
-    version="0.4.7",
+    version="0.4.8",
     author="Igor Gorschal",
     author_email="gorschal@gmail.com",
     description="open source implementation of summly",
