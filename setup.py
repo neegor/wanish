9e3af80ba54b26d9a3eecfcb37eaecedaa2514d6
gorsc <gorschal@gmail.com>
2016-05-29 23:44:37 +0300

    Support Python 3.5

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Software Development :: Pre-processors",
         "Topic :: Text Processing :: Filters",
